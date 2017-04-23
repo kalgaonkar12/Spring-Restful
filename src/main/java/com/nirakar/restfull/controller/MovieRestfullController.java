267f1abb3980050c4812539ca5d3bac07dc5d9a8
Nirakar Mohanty <nirakarmohanty@gmail.com>
2017-04-23 15:23:48 +0530

    updated code with proper comment

--- src/main/java/com/nirakar/restfull/controller/MovieRestfullController.java
+++ src/main/java/com/nirakar/restfull/controller/MovieRestfullController.java
@@ -14,13 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nirakar.movie.data.Movie;
 import com.nirakar.movie.helper.MovieSearchHelper;
 
+/**
+ * This class is used as Restfull controller
+ * @author NIRAKAR
+ *
+ */
 @RestController
-// @Component
 public class MovieRestfullController {
 
 	@Autowired
 	MovieSearchHelper movieSearchHelper;
 
+	/**
+	 * Method used as getting Movie details by using rank number
+	 * @param rank
+	 * @return
+	 */
 	@RequestMapping(value = "/GetRank/{id}", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<Movie> getMovieByRank(@PathVariable("id") int rank) {// ,
 																				// @RequestBody
@@ -37,13 +46,7 @@ public class MovieRestfullController {
 	}
 
 	@RequestMapping(value = "/GetYear/{id}", method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<Movie> getMovieByYear(@PathVariable("id") int year) {// ,
-																				// @RequestBody
-																				// Employee
-																				// employee)
-																				// {
-
-		// MovieSearchHelper movieSearchHelper = new MovieSearchHelper();
+	public ResponseEntity<Movie> getMovieByYear(@PathVariable("id") int year) {// @RequestBody Employee employee) {
 		Movie movie = movieSearchHelper.getMovieByYear(year);
 		HttpHeaders headers = new HttpHeaders();
 
@@ -51,13 +54,17 @@ public class MovieRestfullController {
 		return new ResponseEntity<Movie>(movie, headers, HttpStatus.OK);
 	}
 
+	/**
+	 * This method gives a list of Movies in between two years. 
+	 * @param firstY
+	 * @param secondY
+	 * @return
+	 */
 	@RequestMapping(value = "/GetYear/{firstYear}/{secondYear}", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<List<Movie>> getMovieBetweenYear(
 			@PathVariable("firstYear") int firstY,
-			@PathVariable("secondYear") int secondY) {// , @RequestBody Employee
-														// employee) {
-
-		// MovieSearchHelper movieSearchHelper = new MovieSearchHelper();
+			@PathVariable("secondYear") int secondY) {// , @RequestBody Employee employee) {
+	
 		List<Movie> movies = movieSearchHelper.getMovieBetweenYear(firstY,secondY);
 		HttpHeaders headers = new HttpHeaders();
 
